1ac32239b479061630f31ade8c273b3ff05bd814
Jaanik <jaani.kaukonen2@metropolia.fi>
2020-09-30 14:53:49 +0300

    Hamburgerin javascript lisätty (ei toimi)

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,11 @@
 'use strict'
+const navSlide = () => {
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".buttons");
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle("nav-active");
+  });
+}
+
+navSlide();
