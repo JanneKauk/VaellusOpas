b17c7742cbc5ae55352a9102ada13ab8b35130f5
janiap <jani.peltonen3@metropolia.fi>
2020-10-01 21:27:36 +0300

    Muutoksia info-boxiin, footteriin ja testivisuun

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,22 @@
 'use strict'
-const navSlide = () => {
+/*const navSlide = () => {
   const burger = document.querySelector(".hamburger");
   const nav = document.querySelector(".buttons");
 
   burger.addEventListener('click', () => {
     nav.classList.toggle("nav-active");
   });
-}
+}*/
 
-navSlide();
+//navSlide();
+/*function testi () {
+  alert("testi");
+}*/
+
+const burger2 = document.querySelector(".hamburger");
+
+burger2.addEventListener('click', () => {
+  alert("testi");
+  document.querySelector('.hamburger').style = "transform: scale(2);"
+
+});
