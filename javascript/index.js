44529977541946567d227896cf55899fec79cb7b
janiap <jani.peltonen3@metropolia.fi>
2020-10-03 18:37:32 +0300

    menu muutoksia

--- javascript/index.js
+++ javascript/index.js
@@ -12,9 +12,11 @@ const navOpen = () => {
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
+        setTimeout(function(){document.querySelector('.header').style = 'clip-path: polygon(0 0, 100% 0, 100% 100%, 0 86%);';}, 450);
       } else {
         link.style.animation = `buttonsFade 0.5s ease forwards ${index / 5 +
         0.3}s`;
+        document.querySelector('.header').style = 'clip-path: unset;';
       }
     });
     //Burger animation
